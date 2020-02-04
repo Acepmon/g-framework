121f60d0f89f28cd7d0ec1dd2a89b645f85f34b0
batkhuyag <batkhuyag.batjargal@gmail.com>
2020-02-04 13:46:20 +0800

    text fixed

--- resources/views/themes/car-web/includes/section-title.blade.php
+++ resources/views/themes/car-web/includes/section-title.blade.php
@@ -24,8 +24,8 @@
                                     @if ($content->title)
                                         <h1 class="vehicle-title">{{ $content->title }} - {{$content->metaValue('buildYear')}} / {{$content->metaValue('importDate')}}</h1>
                                     @endif
-                                    <p class="vehicle-meta">Гүйлт: {{$content->metaValue('mileageAmount', 0)}} {{$content->metaValue('mileageUnit')}} |
-                                        Шатахуун: {{$content->metaValue('fuelType')}} | Хөдөлгүүрийн багтаамж: {{$content->metaValue('capacityAmount')}} {{$content->metaValue('capacityUnit')}}
+                                    <p class="vehicle-meta">{{$content->metaValue('mileageAmount', 0)}} {{$content->metaValue('mileageUnit')}} |
+                                         {{$content->metaValue('fuelType')}} |  {{$content->metaValue('capacityAmount')}} {{$content->metaValue('capacityUnit')}}
                                     </p>
 
 
