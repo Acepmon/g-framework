fd0f45ff0e97dc74c84c74b292fc898433015f43
batkhuyagm <batkhuyag.batjargal@gmail.com>
2020-02-21 17:36:36 +0800

    other change to mongolia

--- Modules/Car/Database/Seeders/SpecialVehicleTypeTableSeeder.php
+++ Modules/Car/Database/Seeders/SpecialVehicleTypeTableSeeder.php
@@ -16,8 +16,8 @@ class SpecialVehicleTypeTableSeeder extends Seeder
      */
     public function run()
     {
-        $specialVehicles = ['Excavator', 'Dozer', 'Wheel Loader', 'Tower Crane', 'Cargo Crane', 'Pork Lift', 'Tractor', 'Concrete Mixer Truck', 'Concrete boom pump', 
-        'Blasthole drill rigs', 'Tow Truck', 'Tank Lorry', 'Ladder Truck', 'Camping Car', 'Others'];
+        $specialVehicles = ['Blasthole drill rigs','Cargo Crane','Camping Car','Concrete boom pump','Concrete Mixer Truck','Dozer','Excavator','Ladder Truck','Pork Lift','Tank Lorry','Tow Truck','Tower Crane','Tractor','Wheel Loader','Бусад'];
+             
 
         $parent = TaxonomyManager::register('Special Vehicle Type', 'car', null, ['metaKey' => 'carSubType']);
 
